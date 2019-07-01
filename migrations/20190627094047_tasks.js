6a142801e450cf27485a197bafba17d4302eed3d
NandoBruch <nan_d0@hotmail.com>
2019-07-01 10:40:38 -0300

    Gentilezas

--- migrations/20190627094047_tasks.js
+++ migrations/20190627094047_tasks.js
@@ -1,12 +1,12 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('tasks', (table) => {
-    table.increments('id').primary();
-    table.string('title').notNullable();
-    table.string('deion').nullable();
-    table.timeStamps();
+  return knex.schema.createTable("tasks", (table) => {
+        table.increments("oid").primary();
+        table.string("title").notNullable();
+        table.string("description").nullable();
+        table.timestamps();
   });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('tasks');
+  return knex.schema.dropTable("tasks");
 };
